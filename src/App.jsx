1ea332a17a7fe6da1e7bf7e052f389f9c7a5c22e
Uriel-Jrz8 <division_alexis@hotmail.com>
2022-09-01 17:29:55 -0500

    avance

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment,incrementByAmount } from './store/slices/counter';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+  const {counter} = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
+
+
 
   return (
     <div className="App">
@@ -15,18 +23,21 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>count is {counter}  </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => { dispatch( increment()) }}>
+        increment
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+        <button onClick={() => { dispatch( decrement()) }}>
+        decrement
+        </button>
+
+        <button onClick={() => { dispatch( incrementByAmount(2) ) }}>
+        increment by 2
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </div>
   )
 }
